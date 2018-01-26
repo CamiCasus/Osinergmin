6183163649d8f7bec097760b7d1103f992fc768c
CamiCasus <mcarbajal@sigcomt.com>
2018-01-25 22:14:55 -0500

    Avance de estructura de pantallas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/shared/home/home.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { RegistrarComponent } from './components/guia/registrar/registrar.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    HeaderComponent,
+    RegistrarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
