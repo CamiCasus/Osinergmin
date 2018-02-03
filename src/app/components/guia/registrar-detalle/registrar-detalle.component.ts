e471e1fb38493b493b3d5bf9d123d4cd06340788
CamiCasus <mcarbajal@sigcomt.com>
2018-02-02 22:39:52 -0500

    Correcciones en registrar detallle

--- src/app/components/guia/registrar-detalle/registrar-detalle.component.ts
+++ src/app/components/guia/registrar-detalle/registrar-detalle.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { DetalleGuiaEntidad } from '../../../models/detalleGuiaEntidad';
 import { MaestrosService } from '../../../services/maestros.service';
 import { ProductoEntidad } from '../../../models/productoEntidad';
+import { EnvaseEntidad } from '../../../models/envaseEntidad';
 
 @Component({
   selector: 'app-registrar-detalle',
@@ -10,17 +11,24 @@ import { ProductoEntidad } from '../../../models/productoEntidad';
 })
 export class RegistrarDetalleComponent implements OnInit {
 
+  // tslint:disable-next-line:no-input-rename
   @Input('detalle') detalleGuiaActual: DetalleGuiaEntidad;
   productos: ProductoEntidad[];
+  envases: EnvaseEntidad[];
 
   constructor(private _maestrosService: MaestrosService) { }
 
   ngOnInit() {
-    if (this.detalleGuiaActual == null)
+    if (this.detalleGuiaActual == null) {
       this.detalleGuiaActual = new DetalleGuiaEntidad();
+    }
 
     this._maestrosService.getProductos().subscribe(data => {
       this.productos = data;
+    });
+
+    this._maestrosService.getEnvases().subscribe(data => {
+      this.envases = data;
     });
   }
 
