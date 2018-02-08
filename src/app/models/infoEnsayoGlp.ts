098fe13b96034c1f73f82f15f6f32772c199fadd
Carlos Francia Segura <cfrancia@sigcomt.com>
2018-02-08 12:47:34 -0500

    Ensayos

---
+++ src/app/models/infoEnsayoGlp.ts
@@ -0,0 +1,25 @@
+export class infoEnsayoGlp {
+    id: number;
+    observaciones: string;
+    numeroInformeLaboratorio: string;
+    densidadRelativa: number;
+    presionVapor: number;
+    numeroOctanoMotor: string;
+    metanoMol: string;
+    etanoMol: string;
+    etilenoMol: string;
+    propanoMol: number;
+    propilenoMol: string;
+    isobutanoMol: number;
+    nbutanoMol: number;
+    trans2butenoMol: string;
+    butenoMol: string;
+    isobutilenoMol: string;
+    cis2butenoMol: number;
+    isopentanoMol: number;
+    npentanoMol: string;
+    butadienoMol: string;
+    hexano: string;
+    corrosionLaminaCobre: string;
+    determinacionEtilMercaptano: number;
+}
